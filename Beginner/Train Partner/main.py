b9135071ec842f4f23e2b407e1603297905331bb
goldy1992 <goldy13_1992@hotmail.co.uk>
2016-12-14 17:50:26 +0100

    completed train partner challenge

--- Beginner/Train Partner/main.py
+++ Beginner/Train Partner/main.py
@@ -1,5 +1,42 @@
 import sys
 
-arg = sys.argv
+def f(x):
+        if x % 8 == 0:
+            return(str(int(x)-1) + "SL")
+        
+        if x % 8 == 1:
+            return str(int(x)+3) + "LB"
 
-numEjemplos = int(arg[0])
+        if x % 8 == 2:
+            return str(int(x)+3) + "MB"
+
+        if x % 8 == 3:
+            return str(int(x)+3) + "UB"
+
+        if x % 8 == 4:
+            return str(int(x)-3) + "LB"
+
+        if x % 8 == 5:
+            return str(int(x)-3) + "MB"
+
+        if x % 8 == 6:
+            return str(int(x)-3) + "UB"
+
+        if x % 8 == 7:
+            return str(int(x)+1) + "SU"
+
+        return ""
+    
+
+
+numEjemplos = int(sys.stdin.readline()) 
+input = []
+
+for n in range(1, numEjemplos + 1):
+    input.append(int(sys.stdin.readline()))
+
+for n in input:
+    print( str(f(n)) )
+
+
+
